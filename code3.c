898d952a661fd10df726cc4c1e48546421798d39
tahmeed-35 <108896976+tahmeed-35@users.noreply.github.com>
2023-10-01 15:55:21 +0600

    Add files via upload

---
+++ code3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+
+{
+    int a, b;
+    a = 5;
+    b = 10;
+    printf("%d",a);
+    printf("%d",b);
+    return 0;
+}
---
+++ code4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+
+{
+    float c;
+    c = 25.6;
+    char ch3 = 'a';
+
+    printf("%f \n",c);
+    printf("%c \n",ch3);
+
+    return 0;
+}
